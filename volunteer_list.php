e6cf20d14039024dedfc7d1b1ecbe3a35f4e39a6
charlene-44 <charlene.3572@gmail.com>
2025-02-14 12:04:53 +0100

    correction bug survol sur volunteer_list

--- volunteer_list.php
+++ volunteer_list.php
@@ -49,7 +49,7 @@ error_reporting(E_ALL);
                     <i class="fas fa-user-plus mr-3"></i> Ajouter un bénévole
                 </a>
             </li>
-            <li><a href="my_account.php" class="flex items-center py-2 px-3 hover:green-blue-700 rounded-lg"><i
+            <li><a href="my_account.php" class="flex items-center py-2 px-3 hover:bg-green-700 rounded-lg"><i
                             class="fas fa-cogs mr-3"></i> Mon compte</a></li>
                             <div class="mt-6">
     <a href="logout.php" class="w-full bg-red-700 hover:bg-red-500 text-white py-2 rounded-lg shadow-md">
