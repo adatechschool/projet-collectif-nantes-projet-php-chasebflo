c817a5165d8e02b3ac252bbee2531572037372d6
charlene-44 <charlene.3572@gmail.com>
2025-02-18 10:39:46 +0100

    liens ok

--- volunteer_edit_2.php
+++ volunteer_edit_2.php
@@ -45,9 +45,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <li><a href="volunteer_list.php" class="flex items-center py-2 px-3 hover:bg-green-700 rounded-lg">
                 <i class="fa-solid fa-list mr-3"></i> Liste des bénévoles</a>
             </li>
-            <li><a href="user_add.php" class="flex items-center py-2 px-3 hover:bg-green-700 rounded-lg">
-                <i class="fas fa-user-plus mr-3"></i> Ajouter un bénévole</a>
-            </li>
+            <li><a href="collection_add.php" class="flex items-center py-2 px-3 hover:bg-green-700 rounded-lg"><i
+            class="fas fa-plus-circle mr-3"></i> Ajouter une collecte</a></li>
+            
             <li><a href="my_account.php" class="flex items-center py-2 px-3 hover:bg-green-700 rounded-lg">
                 <i class="fas fa-cogs mr-3"></i> Mon compte</a>
             </li>
