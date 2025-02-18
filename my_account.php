e37761f62651405b28ea964e577d258baecf284e
Sebus83 <sebastien.herbiet@gmail.com>
2025-02-18 12:41:14 +0100

    merge

--- my_account.php
+++ my_account.php
@@ -97,6 +97,55 @@ $stmt = $pdo->prepare("SELECT * FROM benevoles WHERE email = ?");
                 </div>
             </form>
         </div>
+<<<<<<< HEAD
+=======
+        <div class="text-red-600 text-center mb-4" id="error-message" style="display:none;">
+            Le mot de passe actuel est incorrect.
+        </div>
+
+        <form id="settings-form" class="space-y-6">
+            <!-- Champ Email -->
+            
+            <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <input type="email" name="email" id="email" value="<?= $email?>" required
+                       class="w-full p-3 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            </div>
+
+            <!-- Champ Mot de passe actuel -->
+            <div>
+                <label for="current_password" class="block text-sm font-medium text-gray-700">Mot de passe
+                    actuel</label>
+                <input type="password" name="current_password" id="current_password" required
+                       class="w-full p-3 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            </div>
+
+            <!-- Champ Nouveau Mot de passe -->
+            <div>
+                <label for="new_password" class="block text-sm font-medium text-gray-700">Nouveau mot de passe</label>
+                <input type="password" name="new_password" id="new_password"
+                       class="w-full p-3 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            </div>
+
+            <!-- Champ Confirmer le nouveau Mot de passe -->
+            <div>
+                <label for="confirm_password" class="block text-sm font-medium text-gray-700">Confirmer le mot de
+                    passe</label>
+                <input type="password" name="confirm_password" id="confirm_password"
+                       class="w-full p-3 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            </div>
+
+            <!-- Boutons -->
+            <div class="flex justify-between items-center">
+                <a href="collection_list.php" class="text-sm text-blue-600 hover:underline">Retour à la liste des
+                    collectes</a>
+                <button type="button" onclick="updateSettings()"
+                        class="bg-cyan-200 hover:bg-cyan-600 text-white px-6 py-2 rounded-lg shadow-md">
+                    Mettre à jour
+                </button>
+            </div>
+        </form>
+>>>>>>> 40c1070 (archivage ok)
     </div>
 </body>
 
